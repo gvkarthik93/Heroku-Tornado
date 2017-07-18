0cf6a674c71670ca718a4a32b2ec53423cf11f03
Karthik <gvkarthik93@gmail.com>
2017-07-18 21:35:48 +0530

    HTTP Server

--- main.py
+++ main.py
@@ -12,35 +12,7 @@ PORT = 8001
 
 class myHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path=="/":
-            self.wfile.write("Hello")
-
-        try:
-            f = open(curdir + sep + self.path, 'rb')
-            mimetype, _ = mimetypes.guess_type(self.path)
-            try:
-                self.send_response(200)
-                self.send_header('Content-type',mimetype)
-                self.end_headers()
-                self.wfile.write(f.read())
-                f.close()
-            except:
-                print ("Connection Aborted: Established connection has been dropped")
-            return
-        except IOError:
-            self.send_error(404,'File Not Found: %s' % self.path)
-
-    def do_POST(self):
-        try:
-            if self.path == "/searchDetails":
-                try:
-                    print ("searchDetail")
-                    self.wfile.write("{searchDetail:Detail}".encode("utf-8"))
-                except:
-                    print ("Unable to fetch search detail")
-
-        except IOError:
-            self.send_error(404,'File Not Found: %s' % self.path)
+        self.write("Heroku is awesome")
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
